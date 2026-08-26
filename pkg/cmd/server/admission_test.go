k8sstormcenter/storage

commit f9a16e2eeaa182a87b2e15741346171c0fc3ffb0

upgrade sample api-server
Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

pkg/cmd/server/start.go
@@ -17,6 +17,7 @@ limitations under the License.
 package server
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net"
@@ -38,11 +39,15 @@ import (
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/apiserver/pkg/endpoints/openapi"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	genericoptions "k8s.io/apiserver/pkg/server/options"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	"k8s.io/component-base/featuregate"
+	utilversion "k8s.io/component-base/version"
 	netutils "k8s.io/utils/net"
 	"zombiezen.com/go/sqlite/sqlitemigration"
 )
@@ -68,6 +73,20 @@ type WardleServerOptions struct {
 	WatchDispatcher *file.WatchDispatcher
 }
 
+func WardleVersionToKubeVersion(ver *version.Version) *version.Version {
+	if ver.Major() != 1 {
+		return nil
+	}
+	kubeVer := utilversion.DefaultKubeEffectiveVersion().BinaryVersion()
+	// "1.2" maps to kubeVer
+	offset := int(ver.Minor()) - 2
+	mappedVer := kubeVer.OffsetMinor(offset)
+	if mappedVer.GreaterThan(kubeVer) {
+		return kubeVer
+	}
+	return mappedVer
+}
+
 // NewWardleServerOptions returns a new WardleServerOptions
 func NewWardleServerOptions(out, errOut io.Writer, osFs afero.Fs, pool *sqlitemigration.Pool, namespace string, watchDispatcher *file.WatchDispatcher) *WardleServerOptions {
 	o := &WardleServerOptions{
@@ -84,6 +103,7 @@ func NewWardleServerOptions(out, errOut io.Writer, osFs afero.Fs, pool *sqlitemi
 		Namespace:       namespace,
 		WatchDispatcher: watchDispatcher,
 	}
+	o.RecommendedOptions.Admission = nil
 	o.RecommendedOptions.Etcd = nil
 
 	// Disable authorization since we are publishing an internal endpoint (that only answers the API server)
@@ -116,11 +136,17 @@ func NewWardleServerOptions(out, errOut io.Writer, osFs afero.Fs, pool *sqlitemi
 
 // NewCommandStartWardleServer provides a CLI handler for 'start master' command
 // with a default WardleServerOptions.
-func NewCommandStartWardleServer(defaults *WardleServerOptions, stopCh <-chan struct{}) *cobra.Command {
+func NewCommandStartWardleServer(ctx context.Context, defaults *WardleServerOptions, skipDefaultComponentGlobalsRegistrySet bool) *cobra.Command {
 	o := *defaults
 	cmd := &cobra.Command{
 		Short: "Launch a wardle API server",
 		Long:  "Launch a wardle API server",
+		PersistentPreRunE: func(*cobra.Command, []string) error {
+			if skipDefaultComponentGlobalsRegistrySet {
+				return nil
+			}
+			return featuregate.DefaultComponentGlobalsRegistry.Set()
+		},
 		RunE: func(c *cobra.Command, args []string) error {
 			if err := o.Complete(); err != nil {
 				logger.L().Error("config not completed", helpers.Error(err))
@@ -132,7 +158,7 @@ func NewCommandStartWardleServer(defaults *WardleServerOptions, stopCh <-chan st
 				return err
 			}
 			logger.L().Debug("config validated")
-			if err := o.RunWardleServer(stopCh); err != nil {
+			if err := o.RunWardleServer(c.Context()); err != nil {
 				logger.L().Error("unable to run server validated", helpers.Error(err))
 				return err
 			}
@@ -140,10 +166,50 @@ func NewCommandStartWardleServer(defaults *WardleServerOptions, stopCh <-chan st
 			return nil
 		},
 	}
+	cmd.SetContext(ctx)
 
 	flags := cmd.Flags()
 	o.RecommendedOptions.AddFlags(flags)
-	utilfeature.DefaultMutableFeatureGate.AddFlag(flags)
+
+	// The following lines demonstrate how to configure version compatibility and feature gates
+	// for the "Wardle" component, as an example of KEP-4330.
+
+	// Create an effective version object for the "Wardle" component.
+	// This initializes the binary version, the emulation version and the minimum compatibility version.
+	//
+	// Note:
+	// - The binary version represents the actual version of the running source code.
+	// - The emulation version is the version whose capabilities are being emulated by the binary.
+	// - The minimum compatibility version specifies the minimum version that the component remains compatible with.
+	//
+	// Refer to KEP-4330 for more details: https://github.com/kubernetes/enhancements/blob/master/keps/sig-architecture/4330-compatibility-versions
+	defaultWardleVersion := "1.2"
+	// Register the "Wardle" component with the global component registry,
+	// associating it with its effective version and feature gate configuration.
+	// Will skip if the component has been registered, like in the integration test.
+	_, wardleFeatureGate := featuregate.DefaultComponentGlobalsRegistry.ComponentGlobalsOrRegister(
+		apiserver.WardleComponentName, utilversion.NewEffectiveVersion(defaultWardleVersion),
+		featuregate.NewVersionedFeatureGate(version.MustParse(defaultWardleVersion)))
+
+	// Add versioned feature specifications for the "BanFlunder" feature.
+	// These specifications, together with the effective version, determine if the feature is enabled.
+	utilruntime.Must(wardleFeatureGate.AddVersioned(map[featuregate.Feature]featuregate.VersionedSpecs{
+		"BanFlunder": {
+			{Version: version.MustParse("1.2"), Default: true, PreRelease: featuregate.GA, LockToDefault: true},
+			{Version: version.MustParse("1.1"), Default: true, PreRelease: featuregate.Beta},
+			{Version: version.MustParse("1.0"), Default: false, PreRelease: featuregate.Alpha},
+		},
+	}))
+
+	// Register the default kube component if not already present in the global registry.
+	_, _ = featuregate.DefaultComponentGlobalsRegistry.ComponentGlobalsOrRegister(featuregate.DefaultKubeComponent,
+		utilversion.NewEffectiveVersion(utilversion.DefaultKubeBinaryVersion), utilfeature.DefaultMutableFeatureGate)
+
+	// Set the emulation version mapping from the "Wardle" component to the kube component.
+	// This ensures that the emulation version of the latter is determined by the emulation version of the former.
+	utilruntime.Must(featuregate.DefaultComponentGlobalsRegistry.SetEmulationVersionMapping(apiserver.WardleComponentName, featuregate.DefaultKubeComponent, WardleVersionToKubeVersion))
+
+	featuregate.DefaultComponentGlobalsRegistry.AddFlags(flags)
 
 	// replace built-in profiling with pprof on port 6060
 	err := flags.Set("profiling", "false")
@@ -165,6 +231,7 @@ func NewCommandStartWardleServer(defaults *WardleServerOptions, stopCh <-chan st
 func (o WardleServerOptions) Validate(args []string) error {
 	errors := []error{}
 	errors = append(errors, o.RecommendedOptions.Validate()...)
+	errors = append(errors, featuregate.DefaultComponentGlobalsRegistry.Validate()...)
 	return utilerrors.NewAggregate(errors)
 }
 
@@ -201,6 +268,9 @@ func (o *WardleServerOptions) Config() (*apiserver.Config, error) {
 	serverConfig.OpenAPIV3Config.Info.Title = "Wardle"
 	serverConfig.OpenAPIV3Config.Info.Version = "0.1"
 
+	serverConfig.FeatureGate = featuregate.DefaultComponentGlobalsRegistry.FeatureGateFor(featuregate.DefaultKubeComponent)
+	serverConfig.EffectiveVersion = featuregate.DefaultComponentGlobalsRegistry.EffectiveVersionFor(apiserver.WardleComponentName)
+
 	if err := o.RecommendedOptions.ApplyTo(serverConfig); err != nil {
 		return nil, err
 	}
@@ -218,7 +288,7 @@ func (o *WardleServerOptions) Config() (*apiserver.Config, error) {
 }
 
 // RunWardleServer starts a new WardleServer given WardleServerOptions
-func (o WardleServerOptions) RunWardleServer(stopCh <-chan struct{}) error {
+func (o WardleServerOptions) RunWardleServer(ctx context.Context) error {
 	config, err := o.Config()
 	if err != nil {
 		return err
@@ -244,12 +314,12 @@ func (o WardleServerOptions) RunWardleServer(stopCh <-chan struct{}) error {
 	}
 
 	server.GenericAPIServer.AddPostStartHookOrDie("start-sample-server-informers", func(context genericapiserver.PostStartHookContext) error {
-		config.GenericConfig.SharedInformerFactory.Start(context.StopCh)
-		o.SharedInformerFactory.Start(context.StopCh)
+		config.GenericConfig.SharedInformerFactory.Start(context.Done())
+		o.SharedInformerFactory.Start(context.Done())
 		return nil
 	})
 
-	return server.GenericAPIServer.PrepareRun().Run(stopCh)
+	return server.GenericAPIServer.PrepareRun().RunWithContext(ctx)
 }
 
 func servePprof() {

go.mod
@@ -28,18 +28,19 @@ require (
 	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.10.0
 	github.com/yl2chen/cidranger v1.0.2
-	go.opentelemetry.io/otel v1.33.0
+	go.opentelemetry.io/otel v1.35.0
 	go.uber.org/multierr v1.11.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/text v0.22.0
-	k8s.io/api v0.32.1
-	k8s.io/apimachinery v0.32.1
-	k8s.io/apiserver v0.32.0
-	k8s.io/client-go v0.32.1
-	k8s.io/code-generator v0.30.2
-	k8s.io/component-base v0.32.1
+	k8s.io/api v0.32.3
+	k8s.io/apimachinery v0.32.3
+	k8s.io/apiserver v0.32.3
+	k8s.io/client-go v0.32.3
+	k8s.io/code-generator v0.32.3
+	k8s.io/component-base v0.32.3
 	k8s.io/klog/v2 v2.130.1
 	k8s.io/kube-openapi v0.0.0-20241105132330-32ad38e42d3f
+	k8s.io/sample-apiserver v0.32.3
 	k8s.io/utils v0.0.0-20241210054802-24370beab758
 	zombiezen.com/go/sqlite v1.4.0
 )
@@ -52,6 +53,7 @@ require (
 )
 
 require (
+	cel.dev/expr v0.19.1 // indirect
 	github.com/NYTimes/gziphandler v1.1.1 // indirect
 	github.com/acobaugh/osrelease v0.1.0 // indirect
 	github.com/anchore/go-logger v0.0.0-20241205183533-4fc29b5832e7 // indirect
@@ -81,12 +83,12 @@ require (
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.12.0 // indirect
-	github.com/evanphx/json-patch v5.9.0+incompatible // indirect
 	github.com/facebookincubator/nvdtools v0.1.5 // indirect
 	github.com/fatih/color v1.17.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/francoispqt/gojay v1.2.13 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
 	github.com/github/go-spdx/v2 v2.3.2 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
@@ -101,20 +103,19 @@ require (
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/go-openapi/validate v0.24.0 // indirect
 	github.com/go-pkgz/expirable-cache/v3 v3.0.0 // indirect
-	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/cel-go v0.20.1 // indirect
+	github.com/google/btree v1.0.1 // indirect
+	github.com/google/cel-go v0.22.0 // indirect
 	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/go-containerregistry v0.20.2 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.24.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.1-vault-7 // indirect
-	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/copier v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
@@ -167,9 +168,10 @@ require (
 	github.com/vishvananda/netns v0.0.5 // indirect
 	github.com/wagoodman/go-partybus v0.0.0-20230516145632-8ccac152c651 // indirect
 	github.com/wagoodman/go-progress v0.0.0-20230925121702-07e42b3cdba0 // indirect
-	go.etcd.io/etcd/api/v3 v3.5.14 // indirect
-	go.etcd.io/etcd/client/pkg/v3 v3.5.14 // indirect
-	go.etcd.io/etcd/client/v3 v3.5.14 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.16 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.16 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.16 // indirect
 	go.mongodb.org/mongo-driver v1.17.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.53.0 // indirect
@@ -177,62 +179,48 @@ require (
 	go.opentelemetry.io/contrib/instrumentation/runtime v0.55.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.6.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v1.30.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.33.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.35.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.33.0 // indirect
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.30.0 // indirect
 	go.opentelemetry.io/otel/log v0.6.0 // indirect
-	go.opentelemetry.io/otel/metric v1.33.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.33.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.35.0 // indirect
 	go.opentelemetry.io/otel/sdk/log v0.6.0 // indirect
-	go.opentelemetry.io/otel/sdk/metric v1.33.0 // indirect
-	go.opentelemetry.io/otel/trace v1.33.0 // indirect
-	go.opentelemetry.io/proto/otlp v1.4.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
 	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
 	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/exp v0.0.0-20241210194714-1829a127f884 // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/net v0.36.0 // indirect
-	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/oauth2 v0.26.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/term v0.29.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
 	golang.org/x/tools v0.28.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20241209162323-e6fa225c2576 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
-	google.golang.org/grpc v1.69.0 // indirect
-	google.golang.org/protobuf v1.35.2 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/grpc v1.71.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.30.2 // indirect
-	k8s.io/gengo/v2 v2.0.0-20240826214909-a7b603a56eb7 // indirect
-	k8s.io/kms v0.30.2 // indirect
+	k8s.io/gengo/v2 v2.0.0-20240911193312-2b36238f13e9 // indirect
+	k8s.io/kms v0.32.3 // indirect
 	modernc.org/libc v1.55.3 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
 	modernc.org/memory v1.8.0 // indirect
 	modernc.org/sqlite v1.34.2 // indirect
-	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.29.0 // indirect
+	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.31.0 // indirect
 	sigs.k8s.io/controller-runtime v0.18.4 // indirect
 	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.5.0 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
-)
-
-replace (
-	//github.com/openvex/go-vex => github.com/slashben/go-vex v0.0.0-20231012123606-f58e5ee0e14e
-	//github.com/prometheus/client_golang => github.com/prometheus/client_golang v1.16.0
-	//github.com/prometheus/common => github.com/prometheus/common v0.44.0
-	google.golang.org/grpc => google.golang.org/grpc v1.56.3
-	k8s.io/api => k8s.io/api v0.0.0-20231101171312-cd0ecb048ea5
-	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20231102051132-bc0a03b4342c
-	k8s.io/apiserver => k8s.io/apiserver v0.0.0-20231101172914-798e645af694
-	k8s.io/client-go => k8s.io/client-go v0.0.0-20231101171620-66e57f767515
-	k8s.io/code-generator => k8s.io/code-generator v0.0.0-20240727175048-b53d16e2b339
-	k8s.io/component-base => k8s.io/component-base v0.0.0-20231101172256-4b808112b779
-	k8s.io/kms => k8s.io/kms v0.0.0-20231101172519-156d7b7a0cca
 )

main.go
@@ -17,7 +17,6 @@ limitations under the License.
 package main
 
 import (
-	"context"
 	"flag"
 	"net/url"
 	"os"
@@ -46,7 +45,7 @@ func main() {
 		klog.SetLogger(zapr.NewLogger(logger))
 	}
 
-	ctx := context.Background()
+	ctx := genericapiserver.SetupSignalContext()
 	clusterData, err := utilsmetadata.LoadConfig("/etc/config/clusterData.json")
 	if err != nil {
 		logger.L().Ctx(ctx).Fatal("load config error", helpers.Error(err))
@@ -68,9 +67,8 @@ func main() {
 	// setup watcher
 	watchDispatcher := file.NewWatchDispatcher()
 
-	stopCh := genericapiserver.SetupSignalHandler()
 	options := server.NewWardleServerOptions(os.Stdout, os.Stderr, osFs, pool, clusterData.Namespace, watchDispatcher)
-	cmd := server.NewCommandStartWardleServer(options, stopCh)
+	cmd := server.NewCommandStartWardleServer(ctx, options, false)
 
 	// cleanup task
 	client, disco, err := cleanup.NewKubernetesClient()

pkg/apiserver/apiserver.go
@@ -43,7 +43,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"k8s.io/apimachinery/pkg/version"
 	"k8s.io/apiserver/pkg/registry/generic"
 	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
@@ -59,7 +58,8 @@ var (
 	Scheme = runtime.NewScheme()
 	// Codecs provides methods for retrieving codecs and serializers for specific
 	// versions and content types.
-	Codecs = serializer.NewCodecFactory(Scheme)
+	Codecs              = serializer.NewCodecFactory(Scheme)
+	WardleComponentName = "storage"
 )
 
 func init() {
@@ -114,11 +114,6 @@ func (cfg *Config) Complete() CompletedConfig {
 	c := completedConfig{
 		cfg.GenericConfig.Complete(),
 		&cfg.ExtraConfig,
-	}
-
-	c.GenericConfig.Version = &version.Info{
-		Major: "1",
-		Minor: "0",
 	}
 
 	c.GenericConfig.MaxRequestBodyBytes = maxRequestBodyBytes

commit 7abf7db9632d853575d4abb39592d31024eaa21e

apiserver: restore NamespaceLifecycle admission (fixes #43)
Regression from f9a16e2e (upgrade sample api-server, v0.0.174): setting
RecommendedOptions.Admission = nil silently disabled the entire admission
chain, so writes into non-existent namespaces persisted. Restore admission
narrowed to the lifecycle plugin — webhook/policy plugins stay off the profile
write hot path; CoreAPI already builds the kube client and namespace informer
the plugin needs, and the storage ClusterRole already grants namespaces
get/list/watch.

TDD: TestConfigWiresAdmissionControl pins the chain is wired;
TestCreateIntoMissingNamespaceRejected pins create-into-missing-namespace →
NotFound and create-into-existing → admitted, against a fake core API serving
the WatchList protocol. Also corrects the rogueartifact registry
Default/Singular resource swap so the kubectl singular alias resolves.

pkg/cmd/server/start.go
@@ -41,6 +41,7 @@ import (
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/version"
+	"k8s.io/apiserver/pkg/admission/plugin/namespace/lifecycle"
 	"k8s.io/apiserver/pkg/endpoints/openapi"
 	"k8s.io/apiserver/pkg/features"
 	genericapiserver "k8s.io/apiserver/pkg/server"
@@ -111,7 +112,17 @@ func NewWardleServerOptions(out, errOut io.Writer, osFs afero.Fs, pool *sqlitemi
 		StorageConfig:   cfg,
 		WatchDispatcher: watchDispatcher,
 	}
-	o.RecommendedOptions.Admission = nil
+	// Admission: lifecycle-only. Upstream f9a16e2e ("upgrade sample api-server")
+	// set Admission = nil, which silently disabled the ENTIRE admission chain —
+	// NamespaceLifecycle included — so writes into non-existent namespaces
+	// persisted (issue #43). Restore admission but narrow the plugin order to
+	// NamespaceLifecycle: the webhook/policy plugins stay off the profile write
+	// hot path. CoreAPI is already enabled, so the kube client + namespace
+	// informer this plugin needs are built by RecommendedOptions.ApplyTo.
+	// NOTE: the storage ServiceAccount needs namespaces get/list/watch or the
+	// plugin's ready-gate rejects every request.
+	o.RecommendedOptions.Admission = genericoptions.NewAdmissionOptions()
+	o.RecommendedOptions.Admission.RecommendedPluginOrder = []string{lifecycle.PluginName}
 	o.RecommendedOptions.Etcd = nil
 
 	// Disable authorization since we are publishing an internal endpoint (that only answers the API server)

pkg/registry/softwarecomposition/rogueartifact/etcd.go
@@ -20,8 +20,8 @@ func NewREST(scheme *runtime.Scheme, storageImpl storage.Interface, optsGetter g
 		NewFunc:                   func() runtime.Object { return &softwarecomposition.RogueArtifact{} },
 		NewListFunc:               func() runtime.Object { return &softwarecomposition.RogueArtifactList{} },
 		PredicateFunc:             MatchRogueArtifact,
-		DefaultQualifiedResource:  softwarecomposition.Resource("rogueartifact"),
-		SingularQualifiedResource: softwarecomposition.Resource("rogueartifacts"),
+		DefaultQualifiedResource:  softwarecomposition.Resource("rogueartifacts"),
+		SingularQualifiedResource: softwarecomposition.Resource("rogueartifact"),
 
 		Storage: genericregistry.DryRunnableStorage{Codec: nil, Storage: storageImpl},
 

pkg/cmd/server/admission_test.go
@@ -1,0 +1,177 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apiserver/pkg/admission"
+	"k8s.io/apiserver/pkg/authentication/user"
+
+	"github.com/kubescape/storage/pkg/apis/softwarecomposition/v1beta1"
+	"github.com/kubescape/storage/pkg/apiserver"
+)
+
+// fakeKubeAPI serves just enough of the core API for the NamespaceLifecycle
+// admission plugin: a namespace list (one existing namespace), a hanging watch,
+// and per-namespace GETs (existing → 200, anything else → 404).
+func fakeKubeAPI(t *testing.T) *httptest.Server {
+	t.Helper()
+	existing := corev1.Namespace{
+		TypeMeta:   metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "existing-ns", ResourceVersion: "1"},
+		Status:     corev1.NamespaceStatus{Phase: corev1.NamespaceActive},
+	}
+	done := make(chan struct{})
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.URL.Path == "/api/v1/namespaces" && r.URL.Query().Get("watch") == "true":
+			// client-go's WatchList protocol: stream the initial state as ADDED
+			// events, mark the end with a bookmark carrying the
+			// k8s.io/initial-events-end annotation, then hold the watch open.
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			enc := json.NewEncoder(w)
+			if r.URL.Query().Get("sendInitialEvents") == "true" {
+				_ = enc.Encode(map[string]any{"type": "ADDED", "object": existing})
+				bookmark := existing
+				bookmark.Annotations = map[string]string{"k8s.io/initial-events-end": "true"}
+				_ = enc.Encode(map[string]any{"type": "BOOKMARK", "object": bookmark})
+			}
+			if f, ok := w.(http.Flusher); ok {
+				f.Flush()
+			}
+			<-done // hold the watch open for the informer
+		case r.URL.Path == "/api/v1/namespaces":
+			list := corev1.NamespaceList{
+				TypeMeta: metav1.TypeMeta{Kind: "NamespaceList", APIVersion: "v1"},
+				ListMeta: metav1.ListMeta{ResourceVersion: "1"},
+				Items:    []corev1.Namespace{existing},
+			}
+			writeJSON(w, http.StatusOK, list)
+		case r.URL.Path == "/api/v1/namespaces/existing-ns":
+			writeJSON(w, http.StatusOK, existing)
+		case r.URL.Path == "/api" || r.URL.Path == "/apis" || r.URL.Path == "/api/v1":
+			// minimal discovery so client-go does not choke
+			writeJSON(w, http.StatusOK, map[string]any{})
+		default:
+			status := metav1.Status{
+				Status:  metav1.StatusFailure,
+				Code:    http.StatusNotFound,
+				Reason:  metav1.StatusReasonNotFound,
+				Message: "namespaces \"ghost\" not found",
+				Details: &metav1.StatusDetails{Kind: "namespaces", Name: "ghost"},
+			}
+			writeJSON(w, http.StatusNotFound, status)
+		}
+	}))
+	t.Cleanup(func() { close(done); srv.Close() })
+	return srv
+}
+
+func writeJSON(w http.ResponseWriter, code int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_ = json.NewEncoder(w).Encode(v)
+}
+
+func writeKubeconfig(t *testing.T, server string) string {
+	t.Helper()
+	dir := t.TempDir()
+	path := filepath.Join(dir, "kubeconfig")
+	cfg := `apiVersion: v1
+kind: Config
+clusters:
+- name: fake
+  cluster: { server: "` + server + `" }
+contexts:
+- name: fake
+  context: { cluster: fake, user: fake }
+current-context: fake
+users:
+- name: fake
+  user: {}
+`
+	require.NoError(t, os.WriteFile(path, []byte(cfg), 0o600))
+	return path
+}
+
+// admissionTestConfig builds the server config against the fake kube API.
+func admissionTestConfig(t *testing.T) *apiserver.Config {
+	t.Helper()
+	srv := fakeKubeAPI(t)
+	o := newTestOptions()
+	o.RecommendedOptions.Authentication = nil // nil-guarded; keeps the unit env off-cluster
+	o.RecommendedOptions.SecureServing.ServerCert.CertDirectory = t.TempDir()
+	o.RecommendedOptions.CoreAPI.CoreAPIKubeconfigPath = writeKubeconfig(t, srv.URL)
+	cfg, err := o.Config()
+	require.NoError(t, err)
+	return cfg
+}
+
+// TestConfigWiresAdmissionControl pins the regression from upstream f9a16e2e
+// ("upgrade sample api-server"): RecommendedOptions.Admission was nil'd, which
+// silently disabled the ENTIRE admission chain — NamespaceLifecycle included —
+// so writes into non-existent namespaces persist (issue #43).
+func TestConfigWiresAdmissionControl(t *testing.T) {
+	cfg := admissionTestConfig(t)
+	require.NotNil(t, cfg.GenericConfig.AdmissionControl,
+		"the admission chain must be wired — a nil chain means namespace lifecycle is not enforced (issue #43)")
+	assert.True(t, cfg.GenericConfig.AdmissionControl.Handles(admission.Create))
+}
+
+// TestCreateIntoMissingNamespaceRejected pins the behavior: CREATE of an spdx
+// object into a namespace that does not exist must be rejected (NotFound, as
+// the kube-apiserver does for core resources), while an existing namespace
+// admits the object.
+func TestCreateIntoMissingNamespaceRejected(t *testing.T) {
+	cfg := admissionTestConfig(t)
+	require.NotNil(t, cfg.GenericConfig.AdmissionControl, "admission chain missing (issue #43)")
+
+	stop := make(chan struct{})
+	defer close(stop)
+	// Wait on the NAMESPACE informer only: the factory also carries the API
+	// priority-and-fairness informers, which the minimal fake API 404s.
+	nsSynced := cfg.GenericConfig.SharedInformerFactory.Core().V1().Namespaces().Informer().HasSynced
+	cfg.GenericConfig.SharedInformerFactory.Start(stop)
+	deadline := time.Now().Add(20 * time.Second)
+	for !nsSynced() {
+		if time.Now().After(deadline) {
+			t.Fatal("namespace informer never synced against the fake kube API")
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	// NamespaceLifecycle implements the MUTATING phase (Admit) — that is where
+	// the real request path enforces namespace existence.
+	mutator, ok := cfg.GenericConfig.AdmissionControl.(admission.MutationInterface)
+	require.True(t, ok)
+	oi := admission.NewObjectInterfacesFromScheme(apiserver.Scheme)
+	gvk := schema.GroupVersionKind{Group: "spdx.softwarecomposition.kubescape.io", Version: "v1beta1", Kind: "ContainerProfile"}
+	gvr := schema.GroupVersionResource{Group: "spdx.softwarecomposition.kubescape.io", Version: "v1beta1", Resource: "containerprofiles"}
+	mkAttrs := func(ns string) admission.Attributes {
+		obj := &v1beta1.ContainerProfile{ObjectMeta: metav1.ObjectMeta{Name: "cp", Namespace: ns}}
+		return admission.NewAttributesRecord(obj, nil, gvk, ns, "cp", gvr, "", admission.Create, nil, false, &user.DefaultInfo{Name: "test"})
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	err := mutator.Admit(ctx, mkAttrs("ghost"), oi)
+	require.Error(t, err, "create into a never-created namespace MUST be rejected")
+	assert.True(t, apierrors.IsNotFound(err), "rejection is NotFound (the plugin returns the live-lookup error verbatim), got: %v", err)
+
+	assert.NoError(t, mutator.Admit(ctx, mkAttrs("existing-ns"), oi),
+		"create into an existing namespace must be admitted")
+}